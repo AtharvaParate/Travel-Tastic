27d98f73f86ca4618122230ed96fa516b7f99fdf
Atharva Parate <94373219+AtharvaParate@users.noreply.github.com>
2024-07-17 21:41:12 +0530

    login-without-ownerAssociation

--- middleware.js
+++ middleware.js
@@ -1,10 +1,18 @@
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    req.session.redirectUrl = req.originalUrl;
     req.flash(
       "error",
       "You are not logged in , Please login to access listing"
     );
     return res.redirect("/login");
+  }
+  next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
   }
   next();
 };
