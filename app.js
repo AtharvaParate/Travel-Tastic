2491288a180c377d867c62c3d73376a4ee3d7035
Atharva Parate <94373219+AtharvaParate@users.noreply.github.com>
2024-07-18 16:11:40 +0530

    author-owner-reviews-listing

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError");
-const { listingSchema } = require("./schema.js");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
-const { isLoggedIn } = require("./middleware.js");
+const { isLoggedIn, isOwner, validateListing } = require("./middleware.js");
 
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
@@ -42,17 +41,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
-
-const validateListing = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
-
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
 
 const sessionOptions = {
   secret: "mysupersecretcode",
@@ -120,7 +108,7 @@ app.get(
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id)
-      .populate("reviews")
+      .populate({ path: "reviews", populate: { path: "author" } })
       .populate("owner");
     if (!listing) {
       req.flash("error", "Listing Does Not Exist!");
@@ -147,6 +135,7 @@ app.post(
 app.get(
   "/listing/:id/edit",
   isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
@@ -162,6 +151,7 @@ app.get(
 app.put(
   "/listings/:id",
   isLoggedIn,
+  isOwner,
   validateListing,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
@@ -175,6 +165,7 @@ app.put(
 app.delete(
   "/listings/:id",
   isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
