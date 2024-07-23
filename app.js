63e0390b307b7776132b038f4d812d05d6d501c1
Atharva Parate <94373219+AtharvaParate@users.noreply.github.com>
2024-07-23 22:26:23 +0530

    commit-error6

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
+const common = require("./node_modules/debug/src/common.js");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
