4c31da3e2d5c5cd4fbc25ef341ed7abea2a946d5
Atharva Parate <94373219+AtharvaParate@users.noreply.github.com>
2024-07-23 22:28:52 +0530

    update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
 
-const common = require("./node_modules/debug/src/common.js");
+const common = require("./debug/src/common.js");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
